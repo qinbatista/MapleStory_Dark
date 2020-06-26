89e4458d460501e902b49b439b1f15ff4eb9358f
qin yu peng <qin.batista@outlook.com>
2020-06-26 16:13:13 +0800

    base

---
+++ assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidbody_robin;
+    public float speed = 8f;
+    public float crouchSpeedDivisor = 3f;
+    float xVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody_robin = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate() {
+        GroundMovement();
+    }
+    void GroundMovement()
+    {
+        xVelocity = Input.GetAxisRaw("Horizontal");//
+        print("xVelocity="+xVelocity);
+        rigidbody_robin.velocity = new Vector2(xVelocity*speed, rigidbody_robin.velocity.y);
+        FilpDirection();
+    }
+    void FilpDirection()
+    {
+        if (xVelocity<0)
+            transform.localScale = new Vector2(-1,1);
+        if (xVelocity>0)
+            transform.localScale = new Vector2(1,1);
+    }
+}
