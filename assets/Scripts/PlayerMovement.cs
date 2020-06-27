c36dda64b618d868d1bb881b73b7a638d1ef9a6f
qin yu peng <qin.batista@outlook.com>
2020-06-28 01:39:49 +0800

    正常爬梯子

--- assets/Scripts/PlayerMovement.cs
+++ assets/Scripts/PlayerMovement.cs
@@ -26,18 +26,28 @@ public class PlayerMovement : MonoBehaviour
     public bool isOnGround;
     public bool isJump;
     public bool isHeadBlocked;
+    public bool isAroundStair;
     public bool isPushed;
+    public bool isHanging;
     float left_stand = 1;
     //enviernment
     public float footOffset  = 0.4f;
     public float headClearance = 0.5f;
+    public float StairClearance = 0.3f;
     public float groundDistance = 0.2f;
+    float playerHeight;
+    public float eyeHeight = 1.5f;
+    public float grabDistance = 0.4f;
+    public float reachOffset = 0.7f;
     public LayerMask groundLayer;
+    public LayerMask PositionMovementLayer;
     float xVelocity;
+    float yVelocity;
     bool jumpPressed;
     bool jumpHeld;
     bool crouchHeld;
     bool isMoving;
+    bool isUping;
     float lastVelocity;
     //collider
     Vector2 colliderStandSize;
@@ -50,6 +60,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rigidbody_robin = GetComponent<Rigidbody2D>();
         coll_robin = GetComponent<BoxCollider2D>();
+        playerHeight = coll_robin.size.y;
         colliderStandSize = coll_robin.size;
         colliderStandOffset = coll_robin.offset;
         ColliderCrushSize = new Vector2(coll_robin.size.x, coll_robin.size.y/2f);
@@ -63,21 +74,23 @@ public class PlayerMovement : MonoBehaviour
         jumpHeld = Input.GetButton("Jump");
         crouchHeld = Input.GetButton("Crouch");
         isMoving = Input.GetButton("Horizontal");
+        isUping = Input.GetButton("Vertical");
     }
     private void FixedUpdate() {
         GroundMovement();
         PhysicsCheck();
         inAirMovement();
+        onStairMovement();
     }
     void PhysicsCheck()
     {
-        RaycastHit2D leftCheck = Raycost(new Vector2(-footOffset,0f), Vector2.down, groundDistance, groundLayer);
-        RaycastHit2D rightCheck = Raycost(new Vector2(footOffset,0f), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D leftCheck = Raycast(new Vector2(-footOffset,0f), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D rightCheck = Raycast(new Vector2(footOffset,0f), Vector2.down, groundDistance, groundLayer);
         if (leftCheck || rightCheck)
             isOnGround = true;
         else
             isOnGround = false;
-        RaycastHit2D headCheck = Raycost(new Vector2(0f, coll_robin.size.y), Vector2.up, headClearance, groundLayer);
+        RaycastHit2D headCheck = Raycast(new Vector2(0f, coll_robin.size.y), Vector2.up, headClearance, groundLayer);
         if(headCheck)
         {
             isHeadBlocked = true;
@@ -86,6 +99,31 @@ public class PlayerMovement : MonoBehaviour
         {
             isHeadBlocked = false;
         }
+        RaycastHit2D stairCheck = Raycast(new Vector2(-0.25f, coll_robin.size.y), Vector2.right, StairClearance, PositionMovementLayer);
+        if(stairCheck)
+        {
+            isAroundStair = true;
+        }
+        else
+        {
+            isAroundStair = false;
+        }
+
+        float directtin = transform.localScale.x;
+        Vector2 grabDir = new Vector2(directtin, 0f);
+        // RaycastHit2D blockedCheck = Raycast(new Vector2(footOffset* directtin, playerHeight), grabDir, grabDistance, groundLayer);
+        // RaycastHit2D wallCheck = Raycast(new Vector2(footOffset* directtin, eyeHeight), grabDir, grabDistance, groundLayer);
+        // RaycastHit2D ledgeCheck = Raycast(new Vector2(reachOffset* directtin, playerHeight), Vector2.down, grabDistance, groundLayer);
+        // print(isHeadBlocked);
+        // if(isHeadBlocked)
+        // {
+        //     rigidbody_robin.velocity = new Vector2(rigidbody_robin.velocity.x,StandardSpeed);
+        // }
+        // if(!isOnGround && rigidbody_robin.velocity.x<0f && ledgeCheck && wallCheck && !blockedCheck)
+        // {
+        //     rigidbody_robin.bodyType  = RigidbodyType2D.Static;
+        //     isHanging = true;
+        // }
     }
     void GroundMovement()
     {
@@ -104,10 +142,6 @@ public class PlayerMovement : MonoBehaviour
 
         if (isCrouch)
             xVelocity /= crouchSpeedDivisor;
-        else if (!isOnGround && isMoving)
-        {
-            xVelocity /= notOnGroundSpeedDivisor;
-        }
 
         if (isOnGround)
         {
@@ -169,10 +203,102 @@ public class PlayerMovement : MonoBehaviour
                 isPushed=true;
             }
         }
+    }
+    void onStairMovement()
+    {
+        //BackDirection();
+        yVelocity = Input.GetAxisRaw("Vertical");
 
+
+        if (isAroundStair && isUping)
+        {
+            float ss = rigidbody_robin.transform.position.x;
+            int sss = (int)rigidbody_robin.transform.position.x;
+            if(Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x))>0.551 && lastVelocity>0)
+            {
+                isHanging = true;
+                rigidbody_robin.bodyType  = RigidbodyType2D.Static;
+                // print("isAroundStair="+isAroundStair); 
+                // print("1ss:"+ss);
+                // print("1sss:"+sss);
+                // print("1ssss:"+Mathf.Floor(rigidbody_robin.transform.position.x));
+                // print("1ssss:s"+Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x)));
+                // print("lastVelocity="+lastVelocity);
+                rigidbody_robin.transform.position = new Vector2(((Mathf.Floor(rigidbody_robin.transform.position.x))+1.55f),rigidbody_robin.transform.position.y);
+            }
+            else if(Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x))<0.551 && lastVelocity>0)
+            {
+                isHanging = true;
+                rigidbody_robin.bodyType  = RigidbodyType2D.Static;
+                // print("isAroundStair="+isAroundStair);
+                // print("2ss:"+ss);
+                // print("2sss:"+sss);
+                // print("2ssss:"+Mathf.Floor(rigidbody_robin.transform.position.x));
+                // print("2ssss:s"+Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x)));
+                rigidbody_robin.transform.position = new Vector2(((Mathf.Floor(rigidbody_robin.transform.position.x))+0.45f),rigidbody_robin.transform.position.y);
+            }
+
+            else if(Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x))<0.449 && lastVelocity<0)
+            {
+                isHanging = true;
+                rigidbody_robin.bodyType  = RigidbodyType2D.Static;
+                // print("isAroundStair="+isAroundStair);
+                // print("3ss:"+ss);
+                // print("3sss:"+sss);
+                // print("3ssss:"+Mathf.Floor(rigidbody_robin.transform.position.x));
+                // print("3ssss:s"+Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x)));
+                rigidbody_robin.transform.position = new Vector2(((Mathf.Floor(rigidbody_robin.transform.position.x))-0.55f),rigidbody_robin.transform.position.y);
+            }
+
+            else if(Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x))>0.449 && lastVelocity<0)
+            {
+                isHanging = true;
+                rigidbody_robin.bodyType  = RigidbodyType2D.Static;
+                // print("isAroundStair="+isAroundStair);
+                // print("4ss:"+ss);
+                // print("4sss:"+sss);
+                // print("4ssss:"+Mathf.Floor(rigidbody_robin.transform.position.x));
+                // print("4ssss:s"+Mathf.Abs(rigidbody_robin.transform.position.x-Mathf.Floor(rigidbody_robin.transform.position.x)));
+                rigidbody_robin.transform.position = new Vector2(((Mathf.Floor(rigidbody_robin.transform.position.x))+0.45f),rigidbody_robin.transform.position.y);
+            }
+        }
+        if(rigidbody_robin.bodyType  == RigidbodyType2D.Static && isAroundStair==false && isOnGround==false)
+        {
+            isHanging=false;
+            isOnGround=true;
+            // print("Dynamic");
+            rigidbody_robin.bodyType  = RigidbodyType2D.Dynamic;
+        }
+        if (jumpPressed&&isHanging)
+        {
+            rigidbody_robin.bodyType  = RigidbodyType2D.Dynamic;
+            isHanging = false;
+            rigidbody_robin.AddForce(new Vector2(0f,jumpForce*0.3f),ForceMode2D.Impulse);
+            rigidbody_robin.velocity = new Vector2( left_stand*(StandardSpeed+5), rigidbody_robin.velocity.y);
+        }
+        if (isHanging&&isUping)
+        {
+            float climbingValue = 0f;
+            if (yVelocity<0)
+            {
+                climbingValue = -0.1f;
+            }
+            else
+            {
+                climbingValue = 0.1f;
+            }
+            rigidbody_robin.transform.position = new Vector2(rigidbody_robin.transform.position.x, rigidbody_robin.transform.position.y+climbingValue);
+            if(isOnGround && yVelocity<0)
+            {
+                rigidbody_robin.bodyType  = RigidbodyType2D.Dynamic;
+                isHanging = false;
+                // rigidbody_robin.AddForce(new Vector2(0f,jumpForce*0.3f),ForceMode2D.Impulse);
+                // rigidbody_robin.velocity = new Vector2( 0, rigidbody_robin.velocity.y);
+            }
+            // rigidbody_robin.velocity = new Vector2(yVelocity*StandardSpeed, rigidbody_robin.velocity.y);
+        }
 
     }
-
     void FilpDirection()
     {
         if (xVelocity<0)
@@ -198,7 +324,7 @@ public class PlayerMovement : MonoBehaviour
         coll_robin.size = colliderStandSize;
         coll_robin.offset = colliderStandOffset;
     }
-    RaycastHit2D  Raycost(Vector2 offset, Vector2 rayDirection, float length, LayerMask layer)
+    RaycastHit2D  Raycast(Vector2 offset, Vector2 rayDirection, float length, LayerMask layer)
     {
         Vector2 pos = transform.position;
         RaycastHit2D hit = Physics2D.Raycast(pos+ offset, rayDirection, length, layer);
